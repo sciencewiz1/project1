0d45c49b4f8d726affe9ed85b48e130e587bd056
David Goehring <dggoeh1@mit.edu>
2013-09-27 15:11:16 -0400

    attempt to fix

--- everybit/bitarray.c
+++ everybit/bitarray.c
@@ -218,6 +218,6 @@ static size_t modulo(const ssize_t n, const size_t m) {
 
 static char bitmask(const size_t bit_index) {
   return 1 << (bit_index);
-  printf("mask %d",bit_index);
+  printf("mask %d",1 << (bit_index));
 }
 
