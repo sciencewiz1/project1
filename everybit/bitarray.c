031c2f5edd5c0ddcb8c2300e6d4ef939fe3f8cab
David Goehring <dggoeh1@mit.edu>
2013-09-30 19:26:10 -0400

    more

--- everybit/bitarray.c
+++ everybit/bitarray.c
@@ -215,7 +215,7 @@ void bitarray_rotate(bitarray_t *const bitarray,
 
 void reverse(bitarray_t *const bitarray, int start, int stop){
 	int j = stop;
-	for(int i = start; i<=j; i++){
+	for(int i = start; i>=j; i++){
 		int temp = bitarray_get(bitarray,j);
 		bitarray_set(bitarray,j,bitarray_get(bitarray,i));
 		bitarray_set(bitarray,i,temp);
