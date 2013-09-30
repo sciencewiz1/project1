d4fb5437da653f7763148200e8deae4b4798e44b
David Goehring <dggoeh1@mit.edu>
2013-09-30 14:43:50 -0400

    attempt 3

--- everybit/bitarray.c
+++ everybit/bitarray.c
@@ -421,11 +421,11 @@ inline static uint32_t bitarray_get_32bit(bitarray_t * const bitarray, int bit_i
   uint32_t left = *(buf32bit + bit_index/32);
   uint32_t right = *(buf32bit + bit_index/32 + 1);
   uint32_t partialIdx = bit_index % 32;
-  uint32_t partialLeft = masks32left[partialIdx] & left;
-  uint32_t partialRight = masks32right[32 - partialIdx] & right;
+  uint32_t partialLeft = masks32left[32 - partialIdx] & left;
+  uint32_t partialRight = masks32right[partialIdx] & right;
   //uint64_t partialLeft = (0xFFFFFFFF << partialIdx) & left;
   //uint64_t partialRight = (0xFFFFFFFF >> (32 - partialIdx)) & right;
-  return (partialLeft >>> partialIdx) | (partialRight << (32 - partialIdx));
+  return (partialLeft << partialIdx) | (partialRight >> (32 - partialIdx));
 }
 
 inline static uint64_t bitarray_get_64bit(bitarray_t * const bitarray, int bit_index) {
